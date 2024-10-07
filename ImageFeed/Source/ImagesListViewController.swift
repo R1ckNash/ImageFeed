1fb4300bf44b24fc19ea663a9fb57479b567d01b
Ilya Lyasin <lyasin.i@mail.ru>
2024-10-07 17:05:27 +0300

    layout fixes

--- ImageFeed/Source/ImagesListViewController.swift
+++ ImageFeed/Source/ImagesListViewController.swift
@@ -25,9 +25,6 @@ final class ImagesListViewController: UIViewController {
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.rowHeight = 200
-        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
     
     //MARK: - Private methods
