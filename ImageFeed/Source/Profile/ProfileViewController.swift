5cf47b2d97b697f020fc636c98b6fde5bebb77a1
Ilya Lyasin <lyasin.i@mail.ru>
2024-10-11 15:19:09 +0300

    add tab bar and profile screen

---
+++ ImageFeed/Source/Profile/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Ilia Liasin on 11/10/2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    @IBOutlet private weak var avatarImageview: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var loginNameLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    
+    @IBOutlet private weak var logoutButton: UIButton!
+    
+    //MARK: - IBActions
+    @IBAction private func didTapLogoutButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
